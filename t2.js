ffa4fd8ff3a2e9dd79e779da2b0157867d9001cf
dotnetCarpenter <jon.ronnenberg@gmail.com>
2021-08-25 21:34:36 +0200

    unfoldr_ which uses Either

--- t2.js
+++ t2.js
@@ -51,37 +51,37 @@ S.pipe
 
 /** @param {Buffer} buffer */
 function main (buffer) {
-  // gifReader :: Buffer -> Either GifReader
+  //    gifReader :: Buffer -> Either GifReader
   const gifReader = S.encase (buffer => new gif.GifReader (buffer));
 
-  // image :: Number -> Number -> Either Image
+  //    image :: Number -> Number -> Either Image
   const image = height => S.encase (width => new Image (height, width));
 
-  // frameInfo :: GifReader -> Number -> Either FrameInfo
+  //    frameInfo :: GifReader -> Number -> Either FrameInfo
   const frameInfo = gifReader => S.encase (gifReader.frameInfo.bind (gifReader));
 
-  // decodeAndBlitFrameRGBA :: GifReader -> frameInfo -> Number -> Buffer
+  //    decodeAndBlitFrameRGBA :: GifReader -> frameInfo -> Number -> Buffer
   const decodeAndBlitFrameRGBA = gifReader => frameInfo => frameNumber =>
     S.encase (buffer => (gifReader.decodeAndBlitFrameRGBA (frameNumber, buffer), buffer))
       (Buffer.alloc (frameInfo.height * frameInfo.width * 4));
 
-  // addFrame :: FrameInfo -> Buffer -> Image -> Image
+  //    IMPURE_addFrame :: FrameInfo -> Buffer -> Image -> Image
   const IMPURE_addFrame = frameInfo => rgbaBuffer => image => (image.addFrame (rgbaBuffer, frameInfo.delay * 10), image);
 
-  // result1 :: Right (Right FrameInfo)
+  //  result1 :: Right (Right FrameInfo)
   let result1 = S.lift2
     (frameInfo)
     (gifReader (buffer))
     (S.Right (0))
 
-  // result2 :: Right (Right Buffer)
+  //  result2 :: Right (Right Buffer)
   let result2 = S.lift3
     (decodeAndBlitFrameRGBA)
     (gifReader (buffer))
     (S.join (result1))
     (S.Right (0))
 
-  // result3 :: Right Image
+  //  result3 :: Right Image
   let result3 = S.pipe
     ([
       gifReader,
@@ -90,11 +90,45 @@ function main (buffer) {
     ])
     (buffer)
 
+  const fakeRgbaBuffer = frameNumber => frameNumber < 30
+                                          ? S.Right (Buffer.alloc (1))
+                                          : S.Left (new Error ("Frame index out of range."));
+
+  let result4 = unfoldr_ (n => {
+    let buffer = fakeRgbaBuffer (n)
+    return S.isRight (buffer) ? S.map (buf => S.Pair (buf) (n + 1)) (buffer) : buffer
+  }) (0);
+  // let result4 = unfoldr_ (S.compose (S.unchecked.when (S.isRight) (S.map (n => S.Pair (n) (n + 1)))) (fakeRgbaBuffer)) (0);
+  // let result4 = unfoldr_ (n => S.isRight (fakeRgbaBuffer (n))
+  //                               ? S.Right (S.Pair (n) (n + 1))
+  //                               : n)
+  //                        (0);
+
+  //    getFrameInfo :: Either Number -> Either FrameInfo
+  const getFrameInfo = S.compose (S.join)
+                                 (S.lift2
+                                   (frameInfo)
+                                   (gifReader (buffer)));
+
+  //  How do I `S.join` the output of a partially applied function?
+  //    getRgbaBuffer :: Either FrameInfo -> Either Number -> Either (Either Buffer)
+  const getRgbaBuffer = S.lift3 (decodeAndBlitFrameRGBA)
+                                (gifReader (buffer));
+
+
   writeln (result1);
   writeln ();
   writeln (S.map (S.map (showBuffer)) (result2));
   writeln ();
   writeln (result3);
+  writeln ();
+  writeln (result4);
+
+  writeln ();
+  writeln (getFrameInfo (S.Right (29)));
+  writeln ();
+  writeln (S.map (showBuffer) (S.join (getRgbaBuffer (getFrameInfo (S.Right (29))) (S.Right (29)))));
+
 
   // S.pipe
   //   ([
@@ -107,8 +141,17 @@ function main (buffer) {
 function unfoldr_ (f) {
   return x => {
     const result = [];
-    for (var m = f (x); m.isRight; m = f (m.value)) {
-      result.push (m.value);
+    for (var m = f (x); m.isRight; m = f (m.value.snd)) {
+      result.push (m.value.fst);
+    }
+    return result;
+  };
+}
+function unfoldr(f) {
+  return function(x) {
+    var result = [];
+    for (var m = f (x); m.isJust; m = f (m.value.snd)) {
+      result.push (m.value.fst);
     }
     return result;
   };
