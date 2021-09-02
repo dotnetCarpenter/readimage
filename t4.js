c8e934ebe496ca49cb4df5657b78015cef8947fa
dotnetCarpenter <jon.ronnenberg@gmail.com>
2021-09-02 12:24:12 +0200

    Again, 30 decoded and RGBA blit GIF frame buffers

--- t4.js
+++ t4.js
@@ -6,10 +6,10 @@ const S = require ("sanctuary");
 const gif = require ("omggif");
 const fs = require ("fs");
 
-const shortenBuffer = buffer => buffer instanceof Buffer ? `<Buffer(${buffer.byteLength})>` : buffer;
+const consoleFriendlyBuffer = buffer => buffer instanceof Buffer ? `<Buffer(${buffer.byteLength})>` : buffer;
 const writeln = (...xs) => (console.log (...xs), xs[0]);
 
-//    unfoldr_ :: (b -> Either Pair a b) -> b -> Array a
+//    unfoldr_ :: (b -> Either e (Pair a b)) -> b -> Array a
 const unfoldr_ = f => x => {
   const result = [];
   for (var m = f (x); m.isRight; m = f (m.value.snd)) {
@@ -50,13 +50,16 @@ function Frame(rgba, delay) {
   if (!(this instanceof Frame)) {
     return new Frame(rgba, delay)
   }
-  this.data = shortenBuffer (rgba) // because seeing a Buffer in the terminal sucks
+  this.data = consoleFriendlyBuffer (rgba) // because seeing a Buffer in the terminal sucks
   this.delay = delay
 }
 
 const readFile = filename => fs.readFileSync(__dirname + "/" + filename);
 
-//    joinAfter :: Semigroupoid s, Chain m => ???
+// Semigroupoid s => s b c -> s a b -> s a c
+// join :: Chain m => m (m a) -> m a
+
+//    joinAfter :: Semigroupoid s, Chain m => s b c -> s m a m (m b) -> s m a m c
 const joinAfter = S.compose (S.compose (S.join));
 
 //    gifReader :: Buffer -> Either Error GifReader
@@ -76,21 +79,32 @@ const decodeAndBlitFrameRGBA = gifReader => frameNumber => frameInfo =>
 //    IMPURE_addFrame :: FrameInfo -> Buffer -> Image -> Image
 const IMPURE_addFrame = frameInfo => rgbaBuffer => image => (image.addFrame (rgbaBuffer, frameInfo.delay * 10), image);
 
-//    getFrameInfo :: Either Error GifReader -> Either Error Number -> Either Error FrameInfo
+//    getFrameInfo :: Either Error GifReader -> Either Void Number -> Either Error FrameInfo
 const getFrameInfo = joinAfter (S.lift2 (frameInfo));
 
-//    getRgbaBuffer :: Either Error GifReader -> Either Error Number -> Either Error FrameInfo -> Either Error Buffer
+//    getRgbaBuffer :: Either Error GifReader -> Either Void Number -> Either Error FrameInfo -> Either Error Buffer
 const getRgbaBuffer = S.compose (joinAfter)
                                 (S.lift3 (decodeAndBlitFrameRGBA));
-//    blitBuffer :: Either Error GifReader -> Either Error Number -> Either Error Buffer
-const blitBuffer = S.lift2 (S.ap) (getRgbaBuffer) (getFrameInfo);
+
+//    blitFrame :: Either Error GifReader -> Either Void Number -> Either Error Buffer
+const blitFrame = S.lift2 (S.ap) (getRgbaBuffer) (getFrameInfo);
 
 
 /** @param {Buffer} buffer */
 function main (buffer) {
-  //  result1 :: Either Error Buffer
-  let result1 = blitBuffer (gifReader (buffer)) (S.Right (0));
-  return S.map (shortenBuffer) (result1);
+  //    blitGifFrame :: Either Void Number -> Either Error Buffer
+  const blitGifFrame = blitFrame (gifReader (buffer));
+  // return S.map (consoleFriendlyBuffer) (blitGifFrame (S.Right (0)));
+
+  let result = unfoldr_ (n => (
+    S.map (buffer => S.Pair (buffer)
+                            (n + 1))
+          (blitGifFrame (S.Right (n))))
+  )
+  (0);
+
+  // return result;
+  return S.map (consoleFriendlyBuffer) (result);
 }
 
 S.pipe ([ readFile, main, writeln ])
